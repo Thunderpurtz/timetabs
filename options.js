27ed573e2c5c3ea44e5becf053fd959742ae322e
Zenith00 <zenith00dev@gmail.com>
2017-12-12 20:45:43 -0600

    Initial Commit

--- options.js
+++ options.js
@@ -1,12 +1,28 @@
-var toggle = chrome.storage.sync.get("timetab");
-$("#timetab_check").click(function() {
-        $("input[type=checkbox]").attr("checked",!toggle);
-        chrome.storage.sync.set({"timetab":!toggle});
-        toggle = !toggle;
-        console.log("logging")
-});
-
-window.onload = function(e){
-    document.getElementById("timetab_check").checked = chrome.storage.sync.get("timetab");
-    console.log("fsda")
+// Saves options to chrome.storage
+function save_options() {
+  var likesColor = document.getElementById('like').checked;
+  chrome.storage.sync.set({
+    enabled: likesColor
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
 }
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    enabled: true
+  }, function(items) {
+    document.getElementById('like').checked = items.enabled;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
--- rearrange.js
+++ rearrange.js
@@ -1,15 +1,24 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    var tabCount = {
-        all: 0,
-        pinned: 0
-    };
-    var countAll = function (tabs) {
-        tabCount.all = tabs.length;
-    };
-    chrome.tabs.query({currentWindow: true}, countAll);
-    chrome.tabs.move(tabId, {index: tabCount.all});
+    chrome.storage.sync.get({
+        enabled: false
+    }, function (items) {
+        if (items.enabled) {
 
-});
+        var tabCount = {
+            all: 0,
+            pinned: 0
+        };
+        var countAll = function (tabs) {
+            tabCount.all = tabs.length;
+        };
+        chrome.tabs.query({currentWindow: true}, countAll);
+        chrome.tabs.move(tabId, {index: tabCount.all});
+    }
+    });
+
+
+})
+;
 
 chrome.tabs.onCreated.addListener(function (tab) {
     chrome.extension.getBackgroundPage().console.log('create');
